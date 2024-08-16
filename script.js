0859145e61a977f2cfeaa3c12ef2440bc1bc7513
GOWTHAMTH1 <gowthamthangavel1@gmail.com>
2024-08-16 12:23:29 -0700

    files are ready

---
+++ script.js
@@ -0,0 +1,69 @@
+/*js for product details */
+/*js for metaquest in product details */
+var metaquest=document.getElementById("meta-quest");
+var smallmetaquest=document.getElementsByClassName("sm-metaquest");
+smallmetaquest[0].onclick=function(){
+    metaquest.src=smallmetaquest[0].src;
+}
+smallmetaquest[1].onclick=function(){
+    metaquest.src=smallmetaquest[1].src;
+}
+smallmetaquest[2].onclick=function(){
+    metaquest.src=smallmetaquest[2].src;
+}
+smallmetaquest[3].onclick=function(){
+    metaquest.src=smallmetaquest[3].src
+}
+/*js for Gray-shirt in product details*/
+var darkgray=document.getElementById("darkgray");
+var smallgrayshirt=document.getElementsByClassName("sm-grayshirt");
+smallgrayshirt[0].onclick=function(){
+    darkgray.src=smallgrayshirt[0].src;
+}
+smallgrayshirt[1].onclick=function(){
+    darkgray.src=smallgrayshirt[1].src;
+}
+smallgrayshirt[2].onclick=function(){
+    darkgray.src=smallgrayshirt[2].src;
+}
+smallgrayshirt[3].onclick=function(){
+    darkgray.src=smallgrayshirt[3].src;
+}
+/*js for Gray-shirt in product details */
+var earings=document.getElementById("earings");
+var smallearings=document.getElementsByClassName("sm-earings");
+smallearings[0].onclick=function(){
+    earings.src=smallearings[0].src;
+}
+smallearings[1].onclick=function(){
+    earings.src=smallearings[1].src;
+}
+smallearings[2].onclick=function(){
+    earings.src=smallearings[2].src;
+}
+smallearings[3].onclick=function(){
+    earings.src=smallearings[3].src;
+}
+/*js for Gray-shirt in product details */
+var redtop=document.getElementById("redtop");
+var smallredtop=document.getElementsByClassName("sm-redtop");
+smallredtop[0].onclick=function(){
+    redtop.src=smallredtop[0].src;
+}
+smallredtop[1].onclick=function(){
+    redtop.src=smallredtop[1 ].src;
+}
+smallredtop[2].onclick=function(){
+    redtop.src=smallredtop[2].src;
+}
+smallredtop[3].onclick=function(){
+    redtop.src=smallredtop[3].src;
+}
+
+
+
+
+
+
+
+
